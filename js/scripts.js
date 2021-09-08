b769fd6e5c4f95aaf41475e95d1a07f944835ce8
Vu Viet <vudinhviett@gmail.com>
2021-09-09 00:56:44 +0700

    a

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,20 @@
+document.addEventListener('contextmenu', event => event.preventDefault());
+function disableselect(e) {
+    return false;
+}
+
+function reEnable() {
+    return true;
+}
+
+document.onselectstart = new Function("return false");
+
+if (window.sidebar) {
+    document.onmousedown = disableselect;
+    document.onclick = reEnable;
+}
+
 (function($) {
-    "use strict"; 
 	
 	
 	/* Navbar Scripts */
