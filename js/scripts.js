e3cfa2968336ced306e55da83f29160f4ab615bd
Vu Viet <vudinhviett@gmail.com>
2021-10-30 22:07:33 +0700

    big update

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,1 @@
-(function($) {
-    $(window).on('scroll load', function() {
-		if ($(".navbar").offset().top > 20) {
-			$(".fixed-top").addClass("top-nav-collapse");
-		} else {
-			$(".fixed-top").removeClass("top-nav-collapse");
-		}
-    });
-    $(".navbar-nav li a").on("click", function(event) {
-    if (!$(this).parent().hasClass('dropdown'))
-        $(".navbar-collapse").collapse('hide');
-    });
-})(jQuery);
+(function($) {$(window).on('scroll load', function() {		if ($(".navbar").offset().top > 20) {			$(".fixed-top").addClass("top-nav-collapse");		} else {			$(".fixed-top").removeClass("top-nav-collapse");		}});$(".navbar-nav li a").on("click", function(event) {if (!$(this).parent().hasClass('dropdown'))$(".navbar-collapse").collapse('hide');});})(jQuery);
